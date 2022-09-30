626c8526aa074f28a503a52683edcb1036a5cf0a
Danda Rohani <114565030+danrohangit@users.noreply.github.com>
2022-09-30 19:57:04 +0800

    date retrieval and string split
    1) Put get date into function
    2) Create compare cur date to booking date
    3) If 350 days more than cur date, deny booking

--- C++_Simple_Booking_App.cpp
+++ C++_Simple_Booking_App.cpp
@@ -1,10 +1,100 @@
 #include <iostream>;
+#include <list>;
+#include <string>;
+#include <chrono>;
+#include <time.h>;
+#include <string>;
+
+//https://www.javatpoint.com/how-to-split-strings-in-cpp
+
+// # TODO
+
+//1) Put get date into function
+//2) Create compare cur date to booking date
+//3) If 350 days more than cur date, deny booking
 
 using namespace std;
 
+class theList{
+    public:
+        int dD;
+        int mM;
+        int yY;
+        string name;
+        bool checkedIn;
+};
+
 void addBooking(string userOption){
+
     cout << "Book Musicians\n";
-    cout << "-------------------\n\n";
+    cout << "-------------------\n";
+
+    list<theList> objElem;
+
+    string theDateElems[5];
+
+    int tempDate;
+    int tempMonth;
+    int tempYear;
+    string tempName;
+
+    //cout << "\nInsert MONTH in MM format!: ";
+    //cin >> tempMonth;
+
+    //cout << "\nInsert DATE in DD format!: ";
+    //cin >> tempDate;    
+
+    //cout << "\nInsert YEAR in YYYY format!: ";
+    //cin >> tempYear;
+
+    // Retrieve current date START ------------------------------------------------------------------------
+    time_t now = time(0);
+
+    string dt = ctime(&now);
+
+    string delim = " ";
+
+    size_t pos = 0;
+    string token1;
+
+    int count = 0; 
+
+    while (( pos = dt.find(delim)) != std::string::npos){
+
+        token1 = dt.substr(0,pos);
+
+        string theMonths[12] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dev"};
+
+        for (int i = 0;i<13;i++){
+            if (token1 == theMonths[i]){
+                if (i<10){
+                    string temp0 = "0";
+                    string temp1 = to_string(i);
+
+                    token1 = temp0 + temp1;
+                }
+                else{
+                    token1 = to_string(i);
+                }
+            }
+        }
+
+        theDateElems[count] = token1;
+
+        count = count + 1;
+
+        dt.erase(0,pos+delim.length());
+    }
+
+    theDateElems[4] = dt;
+
+    // index 1,2,4 = mm/dd/yyyy
+    //for (int i = 0;i<5;i++){
+    //    cout << theDateElems[i] << "\n";
+    //}
+
+    // Retrieve current date END ------------------------------------------------------------------------       
+
 }
 
 void startMenu(string userOption){
